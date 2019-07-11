765ab77f27223b90c11247cb4eecaa3d9973b3c9
WINDOWS 10 <WINDOWS 10@DESKTOP-7SFELJ4>
2019-07-11 22:14:12 +0200

    Add EF Core and migrations

---
+++ src/TimeTrackerPMF/Data/TimeTrackerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTrackerPMF.Domain;
+
+namespace TimeTrackerPMF.Data
+{
+    public class TimeTrackerDbContext: DbContext
+    {
+        public TimeTrackerDbContext(DbContextOptions<TimeTrackerDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+    }
+}
--- src/TimeTrackerPMF/Startup.cs
+++ src/TimeTrackerPMF/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TimeTrackerPMF.Data;
 
 namespace TimeTrackerPMF
 {
@@ -25,6 +20,7 @@ namespace TimeTrackerPMF
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TimeTrackerDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
