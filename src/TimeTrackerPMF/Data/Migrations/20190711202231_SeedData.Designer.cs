72c2b02b3e3cdc62042cef2edf414bc441567631
WINDOWS 10 <WINDOWS 10@DESKTOP-7SFELJ4>
2019-07-11 22:30:59 +0200

    Add SeedData migration

--- src/TimeTrackerPMF/Data/Migrations/20190711202231_SeedData.Designer.cs
+++ src/TimeTrackerPMF/Data/Migrations/20190711202231_SeedData.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimeTrackerPMF.Data;
 
 namespace TimeTrackerPMF.Data.Migrations
 {
     [DbContext(typeof(TimeTrackerDbContext))]
-    partial class TimeTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190711202231_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
